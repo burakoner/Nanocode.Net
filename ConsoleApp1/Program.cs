a0a1b325ad37e86307af815458a995d9964a509f
Burak Öner <info@burakoner.com>
2022-11-05 22:12:13 +0300

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,26 +9,6 @@ namespace ConsoleApp1
     {
         static async Task Main(string[] args)
         {
-            var smtp = new Nanocode.Net.SMTP.SmtpClient(new Nanocode.Net.SMTP.SmtpSender
-            {
-                From = new MailAddress("phalchatha@gmail.com", "Mezun No-Reply"),
-                Host = "smtp.gmail.com",
-                Port = 587,
-                Credentials = new NetworkCredential("phalchatha@gmail.com", "abfmrirxjtcogxsk"),
-                EnableSsl = true,
-            });
-            smtp.Send("Burak Öner<info@burakoner.com>", "Send", @"Hey Chandler,
-
-I just wanted to let you know that Monica and I were going to go play some paintball, you in?
-
--- Joey");
-            await smtp.SendAsync("Burak Öner<info@burakoner.com>", "SendAsync", @"Hey Chandler,
-
-I just wanted to let you know that Monica and I were going to go play some paintball, you in?
-
--- Joey");
-
-
 
 
 
